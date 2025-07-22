3c9a1e848539bbb82464fe2a34b3cf2be9c05079
jisoo <jisoo2809@gamil.com>
2025-07-22 16:36:38 +0900

    mapper annotation으로 바꿈

--- teamProject/src/main/java/com/spring/teamProject/dao/StoreDAO.java
+++ teamProject/src/main/java/com/spring/teamProject/dao/StoreDAO.java
@@ -1,11 +1,25 @@
 package com.spring.teamProject.dao;
 
 import java.util.List;
-//import org.springframework.dao.DataAccessException;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.teamProject.vo.StoreVO;
 
-public interface StoreDAO {
-	public List<StoreVO> SelectStoreRegion(String region);
-	public List<StoreVO> SelectKeywordStore(String keyword);
+@Mapper
+public class StoreDAO {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<StoreVO> SelectStoreRegion(String region) {
+		List<StoreVO> storelist = sqlSession.selectList("mapper.store.SelectStoreRegion", region);
+		return storelist;
+	}
+	
+	public List<StoreVO> SelectKeywordStore(String keyword) {
+		List<StoreVO> storelist = sqlSession.selectList("mapper.store.SelectKeywordStore",keyword);
+		return storelist;
+	}
 }
--- teamProject/src/main/java/com/spring/teamProject/service/StoreService.java
+++ teamProject/src/main/java/com/spring/teamProject/service/StoreService.java
@@ -2,9 +2,25 @@ package com.spring.teamProject.service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.teamProject.dao.StoreDAO;
 import com.spring.teamProject.vo.StoreVO;
 
-public interface StoreService {
-	public List<StoreVO> storeRegionList(String region) throws Exception;
-	public List<StoreVO> keywordSearchStore(String keyword) throws Exception;
+@Mapper
+public class StoreService{
+	@Autowired
+	private StoreDAO storeDAO;
+	
+	public List<StoreVO> storeRegionList(String region) throws Exception {
+		List<StoreVO> storelist = storeDAO.SelectStoreRegion(region);
+		return storelist;
+	}
+	
+	public List<StoreVO> keywordSearchStore(String keyword) throws Exception {
+		List<StoreVO> storelist = storeDAO.SelectKeywordStore(keyword);
+		return storelist;
+	}
+
 }
