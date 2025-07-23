34fbb4aa444e25ade73b0d66c6d60c3b7a66b14b
wkjo <ksd0607829@gmail.com>
2025-07-23 20:54:09 +0900

    프로젝트 의존성 변경

--- teamProject/src/main/java/com/spring/teamProject/TeamProjectApplication.java
+++ teamProject/src/main/java/com/spring/teamProject/TeamProjectApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@MapperScan("com.spring.teamProject.dao")
+@MapperScan("com.spring.teamProject.dao")
 @SpringBootApplication
 public class TeamProjectApplication {
 
