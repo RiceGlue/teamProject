6fdf7eff3151c8e75b72f1da6a6249b9bb194ce7
jeeusu <jisoo2809@gmail.com>
2025-07-25 09:27:53 +0900

    StoreDAO

--- teamProject/src/main/java/com/spring/teamProject/dao/StoreDAO.java
+++ teamProject/src/main/java/com/spring/teamProject/dao/StoreDAO.java
@@ -3,18 +3,18 @@ package com.spring.teamProject.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring.teamProject.vo.StoreVO;
 
 @Mapper
 public interface StoreDAO {
+	
 	public List<StoreVO> SelectStoreRegion(String region);
 	public List<StoreVO> SelectKeywordStore(String keyword);
 	public List<Long> SelectStoreImageRegion(String region);	
-}
 
-@Repository
-public class StoreDAOImpl {
 
 }
